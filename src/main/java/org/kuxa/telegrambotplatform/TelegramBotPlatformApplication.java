fdf18cbdc9fd491ac794289a5ef466e88ec308d9
Dima Kuxa <d.kuxa88@gmail.com>
2026-02-14 11:17:09 -0400

    Initial commit

---
+++ src/main/java/org/kuxa/telegrambotplatform/TelegramBotPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.kuxa.telegrambotplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kuxa/telegrambotplatform/TelegramBotPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kuxa.telegrambotplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
